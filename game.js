25960d0b63646b2d04a41ef1f1320750da03932c
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-01-17 16:44:41 -0500

    Computer choice initial

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("dasd");
+function getComputerChoice() {
+    computerChoice = Math.random() * 10;
+    console.log(computerChoice);
+    if (computerChoice <= 10/3) {
+        return 'Paper';
+    }
+    else if (computerChoice > 10/3 && computerChoice <= 19/3){
+        return 'Rock';
+    }
+    else {
+        return 'Scissors';
+    } 
+}
