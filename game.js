10aa0ffb8b1551f79bcc11ca1b86e828781e03c1
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-01-24 00:08:41 -0500

    Added logic for player if they choose scissors

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
     computerChoice = Math.random() * 10;
-    console.log(computerChoice);
     if (computerChoice <= 10/3) {
         return 'Paper';
     }
@@ -11,3 +10,25 @@ function getComputerChoice() {
         return 'Scissors';
     } 
 }
+
+function roundStart(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    switch (playerSelection) {
+        case ('scissors'):
+            if (computerSelection.toLowerCase() === 'rock') {
+                return "You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
+                " beats " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1);
+            }
+            else if (computerSelection.toLowerCase() === 'paper') {
+                return "You win! " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1) +
+                " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            }
+            else
+                return "Tie."
+    }
+}
+
+console.log(roundStart('scissors','rock'));
+console.log(roundStart('scissors','paper'));
+console.log(roundStart('scissors','scissors'));
