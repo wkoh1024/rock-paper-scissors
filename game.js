908f89ea0b27155c5f9d295c0f1f742a31a21468
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-06-11 14:32:01 -0400

    Added wins counter

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+playerWins = 0;
+computerWins = 0;
 function getComputerChoice() {
     computerChoice = Math.random() * 10;
     if (computerChoice <= 10/3) {
@@ -14,18 +16,48 @@ function getComputerChoice() {
 function roundStart(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
+    
+    function lossText() {
+        computerWins++;
+        return "You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
+        " beats " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1);
+    }
+    function winText() {
+        playerWins++;
+        return "You win! " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1) +
+        " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+    }
+    console.log("Player Wins: " + playerWins + "\nComputer Wins: " + computerWins)
     switch (playerSelection) {
         case ('scissors'):
-            if (computerSelection.toLowerCase() === 'rock') {
-                return "You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
-                " beats " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1);
+            if (computerSelection === 'rock') {
+                return lossText();
             }
-            else if (computerSelection.toLowerCase() === 'paper') {
-                return "You win! " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1) +
-                " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+            else if (computerSelection === 'paper') {
+                return winText();
             }
             else
                 return "Tie."
+        case ('rock'):
+            if (computerSelection === 'paper') {
+                return lossText();
+            }
+            else if (computerSelection === 'scissors') {
+                return winText();
+            }
+            else
+                return "Tie."
+        case ('paper'):
+            if (computerSelection === 'rock') {
+                return winText();
+            }
+            else if (computerSelection === 'scissors') {
+                return lossText();
+            }
+            else
+                return "Tie.";
+        default:
+            return "Invalid choice. Try again."
     }
 }
 
