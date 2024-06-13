88a7339fdb8b34a1871756d163e8103f62c3b1b6
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-06-13 00:38:49 -0400

    Added player prompting playGame added and plays 5 rounds Added break to switch case in playRound

--- game.js
+++ game.js
@@ -1,66 +1,82 @@
-playerWins = 0;
-computerWins = 0;
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    computerChoice = Math.random() * 10;
+    let computerChoice = Math.random() * 10;
     if (computerChoice <= 10/3) {
-        return 'Paper';
+        return 'paper';
     }
     else if (computerChoice > 10/3 && computerChoice <= 19/3){
-        return 'Rock';
+        return 'rock';
     }
     else {
-        return 'Scissors';
+        return 'scissors';
     } 
 }
 
-function roundStart(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    
-    function lossText() {
-        computerWins++;
-        return "You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
-        " beats " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1);
+function getHumanChoice() {
+    let input = prompt("Pick: rock, paper, scissors");
+    input = input.toLowerCase();
+    while (input !== 'scissors' && input !== 'rock' && input !== 'paper') {
+        input = prompt("Invalid input! Try again. Pick: rock, paper, scissors");
+        input = input.toLowerCase();
     }
-    function winText() {
-        playerWins++;
-        return "You win! " + playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1) +
-        " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-    }
-    console.log("Player Wins: " + playerWins + "\nComputer Wins: " + computerWins)
-    switch (playerSelection) {
-        case ('scissors'):
-            if (computerSelection === 'rock') {
-                return lossText();
-            }
-            else if (computerSelection === 'paper') {
-                return winText();
-            }
-            else
-                return "Tie."
-        case ('rock'):
-            if (computerSelection === 'paper') {
-                return lossText();
-            }
-            else if (computerSelection === 'scissors') {
-                return winText();
-            }
-            else
-                return "Tie."
-        case ('paper'):
-            if (computerSelection === 'rock') {
-                return winText();
-            }
-            else if (computerSelection === 'scissors') {
-                return lossText();
-            }
-            else
-                return "Tie.";
-        default:
-            return "Invalid choice. Try again."
-    }
+    return input;
 }
 
-console.log(roundStart('scissors','rock'));
-console.log(roundStart('scissors','paper'));
-console.log(roundStart('scissors','scissors'));
+function playGame() {
+    function playRound(humanChoice, computerSelection) {
+        // loss and win text function reusability
+        function lossText() {
+            computerScore++;
+            return "You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
+            " beats " + humanChoice.charAt(0).toUpperCase()+ humanChoice.slice(1);
+        }
+        function winText() {
+            humanScore++;
+            return "You win! " + humanChoice.charAt(0).toUpperCase()+ humanChoice.slice(1) +
+            " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        }
+        console.log("Player Score: " + humanScore + "\nComputer Score: " + computerScore)
+        switch (humanChoice) {
+            case ('scissors'):
+                if (computerSelection === 'rock') {
+                    console.log(lossText());
+                }
+                else if (computerSelection === 'paper') {
+                    console.log(winText());
+                }
+                else
+                    console.log("Tie.");
+                break;
+            case ('rock'):
+                if (computerSelection === 'paper') {
+                    console.log(lossText());
+                }
+                else if (computerSelection === 'scissors') {
+                    console.log(winText());
+                }
+                else
+                    console.log("Tie.");
+                break;
+            case ('paper'):
+                if (computerSelection === 'rock') {
+                    console.log(winText());
+                }
+                else if (computerSelection === 'scissors') {
+                    console.log(lossText());
+                }
+                else
+                    console.log("Tie.");
+                break;
+            default:
+                return "Invalid choice. Try again."
+        }
+    }
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+}
+
+playGame();
